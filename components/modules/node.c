7cb19143f3e867e3ecdce424a5898e59b028ed2f
Tom Sutcliffe <tomsci@me.com>
2018-11-08 11:51:55 +0000

    TOMSCI: More luaM_* abuses

--- components/modules/node.c
+++ components/modules/node.c
@@ -496,7 +496,7 @@ static int node_compile( lua_State* L )
   strcpy(output, fname);
   // check here that filename end with ".lua".
   if (len < 4 || (strcmp( output + len - 4, ".lua") != 0) ) {
-    luaM_free( L, output );
+    luaM_freemem( L, output, len+1 );
     return luaL_error(L, "not a .lua file");
   }
 
@@ -505,7 +505,7 @@ static int node_compile( lua_State* L )
   NODE_DBG(output);
   NODE_DBG("\n");
   if (luaL_loadfsfile(L, fname) != 0) {
-    luaM_free( L, output );
+    luaM_freemem( L, output, len+1 );
     return luaL_error(L, lua_tostring(L, -1));
   }
 
@@ -516,7 +516,7 @@ static int node_compile( lua_State* L )
   file_fd = vfs_open(output, "w+");
   if (!file_fd)
   {
-    luaM_free( L, output );
+    luaM_freemem( L, output, len+1 );
     return luaL_error(L, "cannot open/write to file");
   }
 
@@ -530,7 +530,7 @@ static int node_compile( lua_State* L )
   }
   vfs_close(file_fd);
   file_fd = 0;
-  luaM_free( L, output );
+  luaM_freemem( L, output, len+1 );
 
   if (result == LUA_ERR_CC_INTOVERFLOW) {
     return luaL_error(L, "value too big or small for target integer type");
