80db9d0c327a32c1d65d4dda32988118763e4dee
Tom Sutcliffe <tomsci@me.com>
2020-05-02 15:23:01 +0100

    TOMSCI: irpwm: support sending repeating codes

--- components/modules/irpwm.c
+++ components/modules/irpwm.c
@@ -16,6 +16,7 @@
 #include <esp_log.h>
 #include <esp_timer.h>
 #include <driver/gpio.h>
+#include "task/task.h"
 
 #define MAX_PULSES 200
 #define IDLE_TIME 200000 // microseconds
@@ -23,14 +24,25 @@
 #define PULSE_MAX INT32_MAX
 #define PULSE_MIN INT32_MIN
 
+// Ideally and theoretically this would be 80 to give us a tick of 1 microsecond
+// (80MHz / 80 = 1us) meaning no translation of times to ticks would be
+// required. However for some reason it has to be half that, ie 40 not 80. But
+// then we need to make the tick 4 times longer so that our maximum time period
+// of ~100ms will fit in a 15-bit number of ticks. Hence our ticks are 4us.
+#define CLK_DIV 40 * 4
+#define US_TO_TICKS(val) ((val) / 4)
+#define TICKS_TO_US(val) ((val) * 4)
+
 typedef struct irpwm_data
 {
   uint64_t last_time;
-  // uint64_t last_timer_time;
-  int32_t pulses[MAX_PULSES]; // microseconds between falling edges, always < IDLE_TIME
+  int32_t buf1[MAX_PULSES];
+  int32_t buf2[MAX_PULSES];
+  int32_t *pulses; // Points to either buf1 or buf2
   uint8_t count;
   uint8_t gpio;
   uint8_t channel; // rmt_channel_t
+  uint8_t async_sending;
   int timer_ref;
   int callback_ref;
 #ifdef CONFIG_PM_ENABLE
@@ -39,19 +51,24 @@ typedef struct irpwm_data
 } irpwm_data;
 
 
-static irpwm_data* dataForPin(lua_State* L, int gpio)
+static void push_data_table(lua_State *L)
 {
-  // table = registry[dataForPin]
-  lua_pushlightuserdata(L, &dataForPin);
+  lua_pushlightuserdata(L, &push_data_table);
   lua_rawget(L, LUA_REGISTRYINDEX);
   if (lua_isnil(L, -1)) {
     lua_pop(L, 1);
     lua_newtable(L);
-    lua_pushlightuserdata(L, &dataForPin);
+    lua_pushlightuserdata(L, &push_data_table);
     lua_pushvalue(L, -2); // dup table
     lua_rawset(L, LUA_REGISTRYINDEX);
     // table is now on top of stack
   }
+}
+
+// Allocates irpwm_data if it doesn't already exist
+static irpwm_data* dataForPin(lua_State* L, int gpio)
+{
+  push_data_table(L);
   lua_rawgeti(L, -1, gpio);
   if (lua_isnil(L, -1)) {
     lua_pop(L, 1);
@@ -60,6 +77,7 @@ static irpwm_data* dataForPin(lua_State* L, int gpio)
     data->gpio = gpio;
     data->timer_ref = LUA_REFNIL;
     data->callback_ref = LUA_REFNIL;
+    data->pulses = data->buf1;
     lua_rawseti(L, -2, gpio); // pops data
     lua_pop(L, 1); // indextable
     return data;
@@ -68,6 +86,25 @@ static irpwm_data* dataForPin(lua_State* L, int gpio)
     lua_pop(L, 2); // result, indextable
     return (irpwm_data *)result;
   }
+}
+
+// Returns NULL if not found
+static irpwm_data* dataForChannel(lua_State* L, int channel)
+{
+  push_data_table(L);
+  lua_pushnil(L);
+  while (lua_next(L, -2)) {
+    irpwm_data *data = (irpwm_data *)lua_touserdata(L, -1);
+    lua_pop(L, 1); // Removes value
+    if (data->channel == channel) {
+      lua_pop(L, 2); // Removes key and table
+      return data;
+    }
+    // Otherwise keep iterating
+  }
+  // If we get here, not found
+  lua_pop(L, 1); // data table
+  return NULL;
 }
 
 static void freeData(lua_State* L, irpwm_data *data)
@@ -89,7 +126,8 @@ static void irpwm_isr(void *ctx)
 {
   irpwm_data *data = (irpwm_data *)ctx;
   if (data->count >= MAX_PULSES) {
-    data->count = 0;
+    // Stop collecting data but don't overwrite existing
+    return;
   }
 
   int64_t t = esp_timer_get_time();
@@ -122,7 +160,7 @@ static void check_err(lua_State *L, esp_err_t err)
     case ESP_ERR_INVALID_ARG: luaL_error(L, "invalid argument");
     case ESP_ERR_INVALID_STATE: luaL_error(L, "internal logic error");
     case ESP_OK: break;
-    default: luaL_error(L, "Error %d!", err);
+    default: luaL_error(L, "ESP Error %d!", err);
   }
 }
 
@@ -130,21 +168,25 @@ static int timer_tick(lua_State *L)
 {
   // Note this potentially accesses data while ISR is running, hence the volatile
   const void *ptr = lua_touserdata(L, lua_upvalueindex(1));
-  volatile irpwm_data *data = (volatile irpwm_data *)ptr;
+  volatile irpwm_data *vdata = (volatile irpwm_data *)ptr;
 
-  if (data->count && esp_timer_get_time() > data->last_time + IDLE_TIME) {
-    // TODO some buffer switching cleverness here?
-    lua_rawgeti(L, LUA_REGISTRYINDEX, data->callback_ref);
-
+  if (vdata->count && esp_timer_get_time() > vdata->last_time + IDLE_TIME) {
+    // Switch buffers quickly with interrupts disabled
+    irpwm_data *data = (irpwm_data *)ptr;
     check_err(L, gpio_intr_disable(data->gpio));
-    lua_createtable(L, data->count, 0);
-    for (int i = 0; i < data->count; i++) {
-      lua_pushinteger(L, data->pulses[i]);
-      lua_rawseti(L, -2, i + 1);
-    }
+    uint8_t count = data->count;
+    int32_t *buf = data->pulses;
+    data->pulses = (buf == data->buf1) ? data->buf2 : data->buf1;
     data->count = 0;
     check_err(L, gpio_intr_enable(data->gpio));
 
+    // Now we can examine buf at our leisure
+    lua_rawgeti(L, LUA_REGISTRYINDEX, data->callback_ref);
+    lua_createtable(L, count, 0);
+    for (int i = 0; i < count; i++) {
+      lua_pushinteger(L, buf[i]);
+      lua_rawseti(L, -2, i + 1);
+    }
     lua_call(L, 1, 0);
   }
 
@@ -181,6 +223,8 @@ static int irpwm_txconfig(lua_State *L)
     return 0;
   }
   int channel = luaL_checkint(L, 2);
+  luaL_argcheck(L, 2, channel >= RMT_CHANNEL_0 && channel < RMT_CHANNEL_MAX, "Bad channel id");
+
 
 #ifdef CONFIG_PM_ENABLE
   if (!data->pm_lock) {
@@ -191,7 +235,7 @@ static int irpwm_txconfig(lua_State *L)
   rmt_config_t config;
   config.rmt_mode = RMT_MODE_TX;
   config.channel = channel;
-  config.clk_div = 40; // 80; // ie tick is 80Mhz/80 = 1us which implifies ticks vs microseconds nicely
+  config.clk_div = CLK_DIV;
   config.gpio_num = gpio;
   config.mem_block_num = 1;
   config.tx_config.loop_en = false;
@@ -214,20 +258,39 @@ static inline void set_item(rmt_item32_t *item, int first, int second)
 {
   if (first > 0) {
     item->level0 = 1;
-    item->duration0 = first;
+    item->duration0 = US_TO_TICKS(first);
   } else {
     item->level0 = 0;
-    item->duration0 = -first;
+    item->duration0 = US_TO_TICKS(-first);
   }
 
   if (second > 0) {
     item->level1 = 1;
-    item->duration1 = second;
+    item->duration1 = US_TO_TICKS(second);
   } else {
     item->level1 = 0;
-    item->duration1 = -second;
+    item->duration1 = US_TO_TICKS(-second);
   }
 }
+
+static int populate_items(lua_State *L, int idx, rmt_item32_t *items, int max_items)
+{
+  int nitems = 0;
+  for (int i = 0; i < max_items; i++) {
+    lua_rawgeti(L, idx, i + 1);
+    int first = lua_tointeger(L, -1);
+    lua_rawgeti(L, idx, i + 2);
+    int second = lua_tointeger(L, -1);
+    lua_pop(L, 2);
+    if (!first || !second) {
+      break;
+    }
+    set_item(items + nitems, first, second);
+    nitems++;
+  }
+  return nitems;
+}
+
 
 // irpwm.send(gpio, pulses)
 static int irpwm_send(lua_State *L)
@@ -239,21 +302,10 @@ static int irpwm_send(lua_State *L)
   check_err(L, esp_pm_lock_acquire(data->pm_lock));
 #endif
 
-  rmt_item32_t *items = (rmt_item32_t *)data->pulses;
-  int nitems = 0;
+  rmt_item32_t *items = (rmt_item32_t *)data->buf1;
   // I don't know why but the first pulse is always halved so insert some dummy data
-  set_item(items + nitems++, -500, -500);
-  for (int i = 0; i < MAX_PULSES; i++) {
-    lua_rawgeti(L, 2, i + 1);
-    int first = lua_tointeger(L, -1);
-    lua_rawgeti(L, 2, i + 2);
-    int second = lua_tointeger(L, -1);
-    lua_pop(L, 2);
-    if (!first || !second) {
-      break;
-    }
-    set_item(items + nitems++, first, second);
-  }
+  set_item(items, -500, -500);
+  int nitems = 1 + populate_items(L, 2, items + 1, MAX_PULSES - 1);
 
   // ESP_LOGI("irpwm", "APB_CLK frequency = %d", rtc_clk_apb_freq_get());
   esp_err_t err = rmt_write_items((rmt_channel_t)data->channel, items, nitems, true);
@@ -263,6 +315,96 @@ static int irpwm_send(lua_State *L)
 #endif
 
   check_err(L, err);
+  return 0;
+}
+
+static void irpwm_tx_end_task(task_param_t param, task_prio_t prio)
+{
+  rmt_channel_t channel = (rmt_channel_t)param;
+  irpwm_data *data = dataForChannel(lua_getstate(), channel);
+  if (data && data->count) {
+    // Indicates there's a repeat to send
+    rmt_item32_t *items = (rmt_item32_t *)data->buf2;
+    esp_err_t err = rmt_write_items(channel, items, data->count, false);
+    if (err) {
+      ESP_LOGW("irpwm", "Error from repeat rmt_write_items: %d", err);
+    }
+  } else if (data && data->async_sending) {
+#ifdef CONFIG_PM_ENABLE
+    esp_pm_lock_release(data->pm_lock);
+#endif
+    data->async_sending = 0;
+  }
+}
+
+static task_handle_t tx_end_task = 0;
+
+static void irpwm_tx_end(rmt_channel_t channel, void * arg)
+{
+  // This is called in ISR context so post a task to handle completion
+  task_post_low(tx_end_task, channel);
+}
+
+// irpwm.sendasync(gpio, pulses, [repeatpulses])
+static int irpwm_sendasync(lua_State *L)
+{
+  int gpio = luaL_checkint(L, 1);
+  irpwm_data* data = dataForPin(L, gpio);
+  if (data->async_sending) {
+    return luaL_error(L, "Cannot send again before the previous send has completed");
+  }
+
+#ifdef CONFIG_PM_ENABLE
+  check_err(L, esp_pm_lock_acquire(data->pm_lock));
+  data->async_sending = 1;
+#endif
+
+  // Use buf1 for send pulses, buf2 for repeats
+  rmt_item32_t *items = (rmt_item32_t *)data->buf1;
+  // I don't know why but the first pulse is always halved so insert some dummy data
+  set_item(items, -500, -500);
+  int nitems = 1 + populate_items(L, 2, items + 1, MAX_PULSES - 1);
+
+  if (lua_isnoneornil(L, 3)) {
+    data->count = 0;
+  } else {
+    rmt_item32_t *reps = (rmt_item32_t *)data->buf2;
+    data->count = populate_items(L, 3, reps, MAX_PULSES);
+  }
+
+  if (!tx_end_task) {
+    tx_end_task = task_get_id(irpwm_tx_end_task);
+  }
+
+  rmt_register_tx_end_callback(irpwm_tx_end, NULL);
+  esp_err_t err = rmt_write_items((rmt_channel_t)data->channel, items, nitems, false);
+
+#ifdef CONFIG_PM_ENABLE
+  if (err) {
+    esp_pm_lock_release(data->pm_lock);
+    data->async_sending = 0;
+  }
+#endif
+
+  check_err(L, err);
+  return 0;
+}
+
+// irpwm.stopsend(gpio)
+static int irpwm_stopsend(lua_State *L)
+{
+  int gpio = luaL_checkint(L, 1);
+  irpwm_data* data = dataForPin(L, gpio);
+  data->count = 0; // Prevents any further repeats
+  check_err(L, rmt_wait_tx_done(data->channel, portMAX_DELAY));
+  if (data->async_sending) {
+    // Ie if the irpwm_tx_end_task hasn't run yet, manually release the lock
+    // and reset so we're immediately ready to send again
+#ifdef CONFIG_PM_ENABLE
+    esp_pm_lock_release(data->pm_lock);
+#endif
+    data->async_sending = 0;
+  }
   return 0;
 }
 
@@ -351,6 +493,8 @@ LROT_BEGIN(irpwm)
   LROT_FUNCENTRY(listen, irpwm_listen)
   LROT_FUNCENTRY(txconfig, irpwm_txconfig)
   LROT_FUNCENTRY(send, irpwm_send)
+  LROT_FUNCENTRY(sendasync, irpwm_sendasync)
+  LROT_FUNCENTRY(stopsend, irpwm_stopsend)
   LROT_FUNCENTRY(and32, irpwm_and32)
   LROT_FUNCENTRY(not32, irpwm_not32)
 LROT_END(irpwm, NULL, 0)
